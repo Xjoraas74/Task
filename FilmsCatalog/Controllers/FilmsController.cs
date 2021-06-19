46eea83b80030de832e1e9fa5e9f13b24f034e44
Xjoraas74 <xjoraas@gmail.com>
2021-06-19 19:50:25 +0700

    Generate films controller

---
+++ FilmsCatalog/Controllers/FilmsController.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using FilmsCatalog.Data;
+using FilmsCatalog.Models;
+
+namespace FilmsCatalog.Controllers
+{
+    public class FilmsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public FilmsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Films
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Films.Include(f => f.UserSender);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Films/Details/5
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var film = await _context.Films
+                .Include(f => f.UserSender)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (film == null)
+            {
+                return NotFound();
+            }
+
+            return View(film);
+        }
+
+        // GET: Films/Create
+        public IActionResult Create()
+        {
+            ViewData["UserSenderId"] = new SelectList(_context.Users, "Id", "Id");
+            return View();
+        }
+
+        // POST: Films/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Description,ReleaseYear,Director,UserSenderId,PosterName,PosterPath")] Film film)
+        {
+            if (ModelState.IsValid)
+            {
+                film.Id = Guid.NewGuid();
+                _context.Add(film);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["UserSenderId"] = new SelectList(_context.Users, "Id", "Id", film.UserSenderId);
+            return View(film);
+        }
+
+        // GET: Films/Edit/5
+        public async Task<IActionResult> Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var film = await _context.Films.FindAsync(id);
+            if (film == null)
+            {
+                return NotFound();
+            }
+            ViewData["UserSenderId"] = new SelectList(_context.Users, "Id", "Id", film.UserSenderId);
+            return View(film);
+        }
+
+        // POST: Films/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, [Bind("Id,Name,Description,ReleaseYear,Director,UserSenderId,PosterName,PosterPath")] Film film)
+        {
+            if (id != film.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(film);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!FilmExists(film.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["UserSenderId"] = new SelectList(_context.Users, "Id", "Id", film.UserSenderId);
+            return View(film);
+        }
+
+        // GET: Films/Delete/5
+        public async Task<IActionResult> Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var film = await _context.Films
+                .Include(f => f.UserSender)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (film == null)
+            {
+                return NotFound();
+            }
+
+            return View(film);
+        }
+
+        // POST: Films/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            var film = await _context.Films.FindAsync(id);
+            _context.Films.Remove(film);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool FilmExists(Guid id)
+        {
+            return _context.Films.Any(e => e.Id == id);
+        }
+    }
+}
